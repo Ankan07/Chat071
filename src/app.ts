de6abdce1a16a373d6e1aa6909c10494ef558d9d
Ankan Mukherjee <ankantech96@gmail.com>
2020-05-17 02:21:19 +0530

    used express cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import { BaseRoutes } from "./v1/routes";
-
+import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/v1", new BaseRoutes().getRouter());
 app.get("/", (req, res) => {
   res.send("You dont know what you're doing, do you?");
