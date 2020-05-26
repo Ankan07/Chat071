126ffc4654be20a236a5a1a3e618ba7b436989ca
Ankan Mukherjee <ankantech96@gmail.com>
2020-05-27 01:01:26 +0530

    minor 2

--- src/Utils/mail.ts
+++ src/Utils/mail.ts
@@ -1,57 +1,47 @@
-import nodemailer from 'nodemailer';
-import jwt from 'jsonwebtoken';
+import nodemailer from "nodemailer";
+import jwt from "jsonwebtoken";
 
-export const mail=async (email:any,type:any,id:any)=>{
-  try{
-    let html='';
+export const mail = async (email: any, type: any, id: any) => {
+  try {
+    let html = "";
     let transport = nodemailer.createTransport({
-        host: "smtp.mailtrap.io",
-        service: 'Gmail',
-        port: 2525,
-        auth: {
-          user: "ankanmukherjee1480@gmail.com",
-          pass: "Sharp@3452"
-        }
-      });
-     
-    const token=jwt.sign(JSON.stringify(id), "awesome-learning");
-     
-      console.log("token",token);
-      if(type=='registration'){
-      
-        html=`<a href='http://localhost:4000/v1/user/confirmation/${token}'>Click here to verify Email</a>`;
-      }
-       
-      if(type=='forgot password'){
+      host: "smtp.mailtrap.io",
+      service: "Gmail",
+      port: 2525,
+      auth: {
+        user: "ankanmukherjee1480@gmail.com",
+        pass: "XXXX",
+      },
+    });
 
-        html=`<a href='http://localhost:4000/v1/user/confirmation/${token}'>Click here to reset password</a>`;
-      }
-      var mailOptions = {
-        from: 'ankanmukherjee1480@gmail.com',
-        to: email,
-        subject: 'Essentials',
-        text: 'Hey',
-        html: html,
+    const token = jwt.sign(JSON.stringify(id), "awesome-learning");
+
+    console.log("token", token);
+    if (type == "registration") {
+      html = `<a href='http://localhost:4000/v1/user/confirmation/${token}'>Click here to verify Email</a>`;
+    }
+
+    if (type == "forgot password") {
+      html = `<a href='http://localhost:4000/v1/user/confirmation/${token}'>Click here to reset password</a>`;
+    }
+    var mailOptions = {
+      from: "ankanmukherjee1480@gmail.com",
+      to: email,
+      subject: "Essentials",
+      text: "Hey",
+      html: html,
       //   attachments: [
       //     {
       //       filename: 'mailtrap.png',
       //       path: __dirname + '/mailtrap.png',
-      //       cid: 'uniq-mailtrap.png' 
+      //       cid: 'uniq-mailtrap.png'
       //     }
       //   ]
-      };
-      
-     const message=await transport.sendMail(mailOptions)
-     return{"message":"success"}
+    };
 
+    const message = await transport.sendMail(mailOptions);
+    return { message: "success" };
+  } catch (error) {
+    return { message: "failure", error: JSON.stringify(error) };
   }
-  catch(error){
-
-  return{"message":"failure","error":JSON.stringify(error)}
-
-  }
-   
-
-}
-
- 
+};
