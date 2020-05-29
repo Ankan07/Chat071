9d1296d4045911864a172d8955a736d97a645b84
Ankan Mukherjee <ankantech96@gmail.com>
2020-05-29 22:54:30 +0530

    are bhai bhai bhai

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 #Get servers list
-set -e
- 
- 
- 
-ankan=$DEPLOY_SERVERS   
-length=${#ankan}
-echo "Length of '$ankan' is $length"
-# ALL_SERVERS=(${DEPLOY_SERVERS//,/ })
-# for server in "${ALL_SERVERS[@]}"
-# do
-#   echo "Deploying information to EC2 and Gitlab"
-#   echo "Deploy project on server ${server}"
-#   ssh ubuntu@${server}  'bash -s' < ./deploy/updateAndRestart.sh
-# done
- 
+set -f
+string=$DEPLOY_SERVERS
+array=(${string//,/ })
+#Iterate servers for deploy and pull last commit
+for i in "${!array[@]}"; do
+  echo "Deploying information to EC2 and Gitlab"
+  echo "Deploy project on server ${array[i]}"
+  ssh ubuntu@${array[i]}  'bash -s' < ./deploy/updateAndRestart.sh
+done
