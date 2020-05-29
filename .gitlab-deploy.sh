48658c949f2e157d2f2857baa3a754fa5ba62450
Ankan Mukherjee <ankantech96@gmail.com>
2020-05-29 19:29:42 +0530

    so

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #Get servers list
+echo $DEPLOY_SERVERS
+
 set -f
-string=$DEPLOY_SERVERS
-array=(${string//,/ })
-echo "you are not my superiori"
-echo array
+ 
+ssh ubuntu@$DEPLOY_SERVERS  'bash -s' < ./deploy/updateAndRestart.sh
 #Iterate servers for deploy and pull last commit
-for i in "${!array[@]}"; do
-  echo "Deploying information to EC2 and Gitlab"
-  echo "Deploy project on server ${array[i]}"
-  ssh ubuntu@${array[i]}  'bash -s' < ./deploy/updateAndRestart.sh
-done
+# for i in "${!array[@]}"; do
+#   echo "Deploying information to EC2 and Gitlab"
+#   echo "Deploy project on server ${array[i]}"
+#   ssh ubuntu@${array[i]}  'bash -s' < ./deploy/updateAndRestart.sh
+# done
