fb872caa06e10e74c1db5564f28aaa92fea1f916
Ankan Mukherjee <ankantech96@gmail.com>
2020-05-30 01:12:25 +0530

    nanan

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/v1", new BaseRoutes().getRouter());
 app.get("/", (req, res) => {
-  res.send("You dont know what you're doing, do you? yo yo yo honey singh ");
+  res.send("Hello breaking bugs");
 });
 
 app.listen(PORT, () => {
