816b39c7bdd1b059b64c25b82d6624bf6d508dc5
Ankan Mukherjee <ankantech96@gmail.com>
2020-05-29 19:50:39 +0530

    nikal

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #Get servers list
-echo $PRIVATE_KEY
-
 set -f
- 
-ssh ubuntu@{$DEPLOY_SERVERS}  'bash -s' < ./deploy/updateAndRestart.sh
+string=$DEPLOY_SERVERS
+echo $string
+array=(${string//,/ })
 #Iterate servers for deploy and pull last commit
-# for i in "${!array[@]}"; do
-#   echo "Deploying information to EC2 and Gitlab"
-#   echo "Deploy project on server ${array[i]}"
-#   ssh ubuntu@${array[i]}  'bash -s' < ./deploy/updateAndRestart.sh
-# done
+for i in "${!array[@]}"; do
+  echo "Deploying information to EC2 and Gitlab"
+  echo "Deploy project on server ${array[i]}"
+  ssh ubuntu@${array[i]}  'bash -s' < ./deploy/updateAndRestart.sh
+done
