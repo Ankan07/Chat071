60620fb2b6e9f7f8de7202c50153a861399f94ac
Ankan Mukherjee <ankantech96@gmail.com>
2020-05-29 21:11:39 +0530

    new

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #Get servers list
-set -f
-string=$DEPLOY_SERVERS
-        
+set -e
+ 
 ankan=mukherjee
-echo $ankan
-array=(${string//,/ })
-#Iterate servers for deploy and pull last commit
-for i in "${!array[@]}"; do
+ 
+DEPLOY_SERVERS=$DEPLOY_SERVERS // We defined this in GitLab
+ALL_SERVERS=(${DEPLOY_SERVERS//,/ })
+for server in "${ALL_SERVERS[@]}"
+ 
   echo "Deploying information to EC2 and Gitlab"
-  echo "Deploy project on server ${array[i]}"
-  ssh ubuntu@${array[i]}  'bash -s' < ./deploy/updateAndRestart.sh
+  echo "Deploy project on server ${server}"
+  ssh ubuntu@${server}  'bash -s' < ./deploy/updateAndRestart.sh
 done
