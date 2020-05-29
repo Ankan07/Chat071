b21b5b5801e8913e6a01f936154f50940e3408e2
Ankan Mukherjee <ankantech96@gmail.com>
2020-05-29 21:33:13 +0530

    new gj dkvnsdgsdg

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -2,14 +2,16 @@
 #Get servers list
 set -e
  
-ankan=mukherjee
  
-DEPLOY_SERVERS=$DEPLOY_SERVERS  
-ALL_SERVERS=(${DEPLOY_SERVERS//,/ })
-for server in "${ALL_SERVERS[@]}"
-do
-  echo "Deploying information to EC2 and Gitlab"
-  echo "Deploy project on server ${server}"
-  ssh ubuntu@${server}  'bash -s' < ./deploy/updateAndRestart.sh
-done
+ 
+ankan=$DEPLOY_SERVERS   
+length=${#ankan}
+echo "Length of '$ankan is $length"
+# ALL_SERVERS=(${DEPLOY_SERVERS//,/ })
+# for server in "${ALL_SERVERS[@]}"
+# do
+#   echo "Deploying information to EC2 and Gitlab"
+#   echo "Deploy project on server ${server}"
+#   ssh ubuntu@${server}  'bash -s' < ./deploy/updateAndRestart.sh
+# done
  
