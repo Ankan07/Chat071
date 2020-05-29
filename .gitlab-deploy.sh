dc451b186eab8b7e00a7e5236ca3db03c95e1595
Ankan Mukherjee <ankantech96@gmail.com>
2020-05-29 18:49:32 +0530

    pipelinehgfhf

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #Get servers list
 echo "in gitlab-deploy.sh"
+echo $DEPLOY_SERVERS
 set -f
+
 string=$DEPLOY_SERVERS
 array=(${string//,/ })
 #Iterate servers for deploy and pull last commit
