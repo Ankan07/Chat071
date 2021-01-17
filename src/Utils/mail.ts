a46c22ab07841e320fa118a2ac4c9ccdd211d1d4
ankan <ankan.mukherjee@riskcovry.com>
2021-01-17 19:52:43 +0530

    chat api

--- src/Utils/mail.ts
+++ src/Utils/mail.ts
@@ -2,18 +2,18 @@ import nodemailer, { TransportOptions } from "nodemailer";
 import jwt from "jsonwebtoken";
 import { google } from "googleapis";
 import SMTPTransport from "nodemailer/lib/smtp-transport";
+import { values } from '../config'
 
 export const mail = async (email: any, type: any, id: any) => {
   try {
     const OAuth2 = google.auth.OAuth2;
     const oauth2Client = new OAuth2(
-      "550151232396-4qvutl9q8mlfip23kq573m95qhlqmp1b.apps.googleusercontent.com",
-      "Gwg3J9IkYLU6K5J9s9viiU2-", // Client Secret
+      values["clientId"],
+      values["clientSecret"], // Client Secret
       "https://developers.google.com/oauthplayground" // Redirect URL
     );
     oauth2Client.setCredentials({
-      refresh_token:
-        "1//04Ay7BN4XEnQqCgYIARAAGAQSNwF-L9IrPWcbKZXRvemWSAoA2ts-yFBosNBcHtOMHGUMuFiqfV1ULEsA8nvaGSCxqK5JIYdj1vk",
+      refresh_token: values["refreshToken"],
     });
 
     const accessToken = await oauth2Client.getAccessToken();
@@ -23,17 +23,14 @@ export const mail = async (email: any, type: any, id: any) => {
       auth: {
         type: "OAuth2",
         user: "ankantech96@gmail.com",
-        clientId:
-          "550151232396-4qvutl9q8mlfip23kq573m95qhlqmp1b.apps.googleusercontent.com",
-        clientSecret: "Gwg3J9IkYLU6K5J9s9viiU2-",
-        refreshToken:
-          "1//04Ay7BN4XEnQqCgYIARAAGAQSNwF-L9IrPWcbKZXRvemWSAoA2ts-yFBosNBcHtOMHGUMuFiqfV1ULEsA8nvaGSCxqK5JIYdj1vk",
+        clientId: values["clientSecret"],
+        clientSecret: values["clientSecret"],
+        refreshToken: values["refreshToken"],
         accessToken: accessToken.token as string,
       },
     });
 
-    // "13.126.82.139"
-    const ip = "localhost";
+    const ip = values["ip"];
     const token = jwt.sign(JSON.stringify(id), "awesome-learning");
 
     let html = "";
